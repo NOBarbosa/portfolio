601a24b82dbde5200232915d6217aa2cbb954852
Natália Barbosa <nob.ufba@gmail.com>
2022-05-15 23:09:04 -0300

    alguns estilos

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,8 +1,32 @@
+import { useState } from 'react';
 import style from './styles.module.scss';
 export function Home(){
+    const [show, setShow] = useState(false);
+
+    const handleClick = () =>{
+        console.log(show)
+        setShow(!show)
+    }
     return(
-        <div>
-            <h1>Home</h1>
+        <div className={style.container}>
+            <div>
+                <h2>Desenvolvedora Fullstack Junior</h2>
+            </div>
+            <div className={style.tech}>
+                    <button onClick={()=>handleClick()} >Tecnologias</button>
+                <ul className={show ? style.visible : style.notVisible}>
+                    <li>HTML</li>
+                    <li>CSS, SCSS</li>
+                    <li>JavaScript Es6+</li>
+                    <li>React</li>
+                    <li>React-router-dom v6</li>
+                    <li>API REST</li>
+                    <li>Material-ui</li>
+                    <li>Nodejs</li>
+                    <li>express</li>
+                    <li>scrum</li>
+                </ul>
+            </div>
         </div>
     )
 }
--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -1,11 +1,12 @@
 import style from './styles.module.scss';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import { NavLink, Outlet, useNavigate} from 'react-router-dom'
 
 export function Main(){
   const navigate = useNavigate()
     return(
-            <div>
+      <div className={style.container}>
             <header className={style.header}>
                 <NavLink to='/projects'> Projetos</NavLink>
                 <h1 onClick={()=>navigate('/')}>Natália Oliveira</h1>
@@ -14,7 +15,10 @@ export function Main(){
             <main className={style.main}>
                 <Outlet/>
             </main>
-            <footer><LinkedInIcon/></footer>
+            <footer className={style.footer}>
+              <a href='https://www.linkedin.com/in/nobarbosa/' target='_blank' rel="noreferrer"><LinkedInIcon /></a>
+              <a href='https://github.com/NOBarbosa' target='_blank' rel="noreferrer"><GitHubIcon/></a>  
+            </footer>
     </div>
     )
 }
